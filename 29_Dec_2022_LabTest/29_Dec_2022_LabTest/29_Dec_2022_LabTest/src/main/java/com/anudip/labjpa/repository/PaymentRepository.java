1008a74a6b779949298ea13db29601b335906c54
Achalbagmare123 <110631189+Achalbagmare123@users.noreply.github.com>
2022-12-30 00:08:17 +0530

    29_Dec_2022_LabTest

---
+++ 29_Dec_2022_LabTest/29_Dec_2022_LabTest/29_Dec_2022_LabTest/src/main/java/com/anudip/labjpa/repository/PaymentRepository.java
@@ -0,0 +1,15 @@
+package com.anudip.labjpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.anudip.labjpa.entity.Payment;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+
+	@Query("Select p from Payment p where p.cName=?1")
+	Payment getPaymentByName(String cName);
+	
+	@Query("Select p from Payment p where p.cPhone")
+	Payment getPaymentByPhone(long cPhone);
+}
